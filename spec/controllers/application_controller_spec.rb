fcbdde915be1b6f8903ff8311fe3fb53a2636f40
Ray Mulligan <rmullig2@optonline.net>
2016-12-09 22:29:57 -0500

    created homepage test

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe ApplicationController do
+
+  describe "Homepage" do
+    it 'loads the homepage' do
+      get '/'
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include("Welcome to Fwitter")
+    end
+  end
+end
