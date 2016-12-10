34feb69e6d56a5ee7d338470edcb994c06476d06
Ray Mulligan <rmullig2@optonline.net>
2016-12-09 22:45:17 -0500

    created and passed first user test

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,9 @@
 require './config/environment'
 
 class UserController < ApplicationController
+  
+  get '/signup' do
+    erb :'/users/create'
+  end
 
 end
--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper'
 
 describe UserController do
   
+  describe "Signup Page" do
+    
+    it 'loads the signup page' do
+      get '/signup'
+      expect(last_response.status).to eq(200)
+    end
+    
+  end
+  
 end
