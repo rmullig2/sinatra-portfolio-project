d9d1b2592655b7e52eda48fd15e1ad42dd9713ae
Ray Mulligan <rmullig2@optonline.net>
2016-12-11 20:42:18 -0500

    added helper method to validate new users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,5 +16,26 @@ class ApplicationController < Sinatra::Base
     session[:fail] = ""
     erb :index
   end
+  
+  helpers do
+    def valid_user?
+      if params[:first_name].empty?
+        session[:fail] = "Please specify first name"
+        false
+      elsif params[:last_name].empty?
+        session[:fail] = "Please specify last name"
+        false
+      elsif params[:user_name].empty?
+        session[:fail] = "Please specify login name"
+        false
+      elsif params[:password].empty?
+        session[:fail] = "Please specify password"
+        false
+      else
+        session[:fail] = ""
+        true
+      end
+    end
+  end
 
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,29 +4,15 @@ class UserController < ApplicationController
   
   get '/signup/?' do
     @failure_message = session[:fail] || @failure_message = ""
-    #binding.pry
     erb :'/users/create'
   end
   
   post '/signup' do
-    #binding.pry
-    if params[:first_name].empty?
-      session[:fail] = "Please specify first name"
-      #binding.pry
-      redirect to '/signup'
-    elsif params[:last_name].empty?
-      session[:fail] = "Please specify last name"
-      redirect to '/signup'
-    elsif params[:user_name].empty?
-      session[:fail] = "Please specify login name"
-      redirect to '/signup'
-    elsif params[:password].empty?
-      session[:fail] = "Please specify password"
-      redirect to '/signup'
-    else
-      session[:fail] = ""
-      redirect to '/'
-    end
+     if valid_user?
+        redirect to '/'
+     else
+       redirect to '/signup'
+     end
   end
 
 end
