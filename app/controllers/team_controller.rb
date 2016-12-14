60d97b9b2a4ff2ac77b57c6811c33691125b3cb5
Ray Mulligan <rmullig2@optonline.net>
2016-12-14 17:15:57 -0500

    create test to check for details page, create details page

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -5,5 +5,10 @@ class TeamController < ApplicationController
   get '/team/home' do
     erb :'/team/home'
   end
+  
+  post '/team/detail' do
+    @team = Team.find_by name: params[:team_name]
+    binding.pry
+  end
 
 end
--- spec/controllers/team_controller_spec.rb
+++ spec/controllers/team_controller_spec.rb
@@ -24,4 +24,13 @@ describe TeamController do
     
   end
   
+  describe 'Details Page' do
+
+    it 'loads the team details' do
+      get '/team/detail'
+      expect(last_response.status).to eq(200)
+    end
+    
+  end
+  
 end
