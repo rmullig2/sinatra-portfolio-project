9f5fd07bc4c7b23c239670ae576ef4984b64e028
Ray Mulligan <rmullig2@optonline.net>
2016-12-13 19:41:38 -0500

    wrote controller method for viewing/deleting users

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -8,6 +8,12 @@ class AdminController < ApplicationController
   end
   
   get '/admin/users' do
+    @user = User.find_by id: session[:id]
+    if @user.id == 1
+        erb :'/admin/users'
+    else
+      redirect to '/logout'
+    end
   end
   
 end
