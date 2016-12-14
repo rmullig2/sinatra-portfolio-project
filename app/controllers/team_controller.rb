fc72fa3fe32ac58d3c310f9bdb9b20531a85247c
Ray Mulligan <rmullig2@optonline.net>
2016-12-14 16:07:58 -0500

    create outline of team home page and write tests to verify

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,5 +1,9 @@
 require './config/environment'
 
 class TeamController < ApplicationController
+  
+  get '/team/home' do
+    erb :'/team/home'
+  end
 
 end
---
+++ spec/controllers/team_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe TeamController do
+  
+  describe 'Standings Page' do
+    
+    it 'loads the team standings' do
+      get '/team/home'
+      expect(last_response.status).to eq(200)
+    end
+    
+    it 'lists each league' do
+      visit '/team/home'
+      page.should have_content("American")
+      page.should have_content("National")
+    end
+    
+    it 'lists each division' do
+      visit '/team/home'
+      page.should have_content("East")
+      page.should have_content("Central")
+      page.should have_content("West")
+    end
+    
+  end
+  
+end
