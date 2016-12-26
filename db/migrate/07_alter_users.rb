ed6fca592e1b4e866f7aba1ff5f664507ed000b7
Ray Mulligan <rmullig2@optonline.net>
2016-12-26 14:03:37 -0500

    add admin boolean field to users table

---
+++ db/migrate/07_alter_users.rb
@@ -0,0 +1,5 @@
+class AlterUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :admin, :boolean, default:  false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 6) do
+ActiveRecord::Schema.define(version: 7) do
 
   create_table "contracts", force: :cascade do |t|
     t.integer "years"
@@ -46,11 +46,12 @@ ActiveRecord::Schema.define(version: 6) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "user_name"
-    t.string "password"
-    t.string "password_digest"
+    t.string  "first_name"
+    t.string  "last_name"
+    t.string  "user_name"
+    t.string  "password"
+    t.string  "password_digest"
+    t.boolean "admin",           default: false
   end
 
 end
